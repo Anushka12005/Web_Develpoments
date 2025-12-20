52c930c4f2cbac586eff82b5df50906a083bf980
Anushka12005 <anushkajamdade43@gmail.com>
2025-12-20 11:37:10 +0530

    Array

---
+++ Java Script/Array/01_Create_An_Array_Of_Five_Student_Names_And_Print_Them_Using_A_Loop.js
@@ -0,0 +1,6 @@
+var Student = ["Anushka","Prachi",Saku,];
+
+for(let i = 0; i<Student.length;i++)
+{
+    console.log(Studnt[i]);
+}
---
+++ Java Script/Array/03_Remove_duplicate_values_from_an_array.js
@@ -0,0 +1,10 @@
+let Numbers = [1,2,3,4,5,8,9];
+
+let mySet = new Set(Number);
+
+console.log(mySet);
+
+let numbers = [1.3,5,6,4,6];
+
+let uniqueNumberss = [...new Set(numbers)];
+console.log(uniqueNumbers);
